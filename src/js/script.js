4d0f1aef68372a5ad00579f3b3ed1e3de2b5dbb4
prerogativa <ilya.blitz.gsg9@gmail.com>
2021-08-22 18:02:59 +0300

    Validation

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@
 //         ]
 //     });
 // });
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -31,3 +31,48 @@ document.querySelector('.slick-prev').onclick = function () {
 document.querySelector('.slick-next').onclick = function () {
     slider.goTo('next');
 };
+
+(function($) {
+    $(function() {
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+              .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+              .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+          });
+    });
+
+
+
+    function toogleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toogleSlide('.catalog-item__link');
+    toogleSlide('.catalog-item__back');
+
+    $('[data-model=consultation]').on('click',function() {
+        $('.overlay, #consult').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consult, #thanks, #order').fadeOut('slow');
+    })
+
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');            
+        })
+    })
+
+    $('#consultation-form').validate();
+    $('#form_1').validate();
+    $('#form_2').validate();
+
+})(jQuery);
